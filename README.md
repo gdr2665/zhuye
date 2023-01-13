4531291882d14cfd92aa47084d02881cbb8651dc
Tianze Ds Qiu <slaaker@126.com>
2023-01-13 21:46:03 +0800

    chores

---
+++ README.md
@@ -0,0 +1,24 @@
+# Frontend
+
+## 项目技术
+
+基于 React，并且使用 TypeScript。在库层面，本项目使用 Tencent TDesign，Ace Editor，Axios 等。
+
+## 项目结构
+
+```
+src/
+    App.tsx：主页
+    components/
+        answered：问答展示模块用户回答主组件，用例见 Problem
+        aske：问答展示模块提问信息展示主组件，用例见 Problem
+    pages/
+        Ask：输入和提交问题的界面
+        Explore：探索界面，暂未规划
+        Login：登陆界面
+        Problem：问答展示模块页面
+        Register：注册界面
+        Report：反馈页面
+        Square：广场，即首页
+        User：用户中心页
+```
--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import User from "./pages/User";
 import Report from "./pages/Report";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Problem from "./pages/Problem";
 
 function App() {
     const {Content, Footer, Aside} = Layout
@@ -27,6 +28,7 @@ function App() {
                     <MenuItem value="1" href={"/ask"}><ThinkingProblem/></MenuItem>
                     <MenuItem value="2" href={"/"}><Palace/></MenuItem>
                     <MenuItem value="3" href={"/explore"}><SignalTower/></MenuItem>
+                    <MenuItem value="3" href={"/problem"}>临时例子</MenuItem>
                     <Divider className={"leftDown"}></Divider>
                     <MenuItem value="4" href={logon ? "/user" : "/register"}><Me/></MenuItem>
                     <MenuItem value="5" href={"/report"}><MessageEmoji/></MenuItem>
@@ -43,6 +45,7 @@ function App() {
                             <Route path="report" element={<Report/>}/>
                             <Route path="login" element={<Login/>}/>
                             <Route path="register" element={<Register/>}/>
+                            <Route path="problem" element={<Problem/>}/>
                         </Routes>
                     </BrowserRouter>
                 </Content>
