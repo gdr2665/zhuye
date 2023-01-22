a15a66ff6e5c02cf62a49522c43bfc645f8ae2c2
aliyun7489356495 <accounts_63a9a18d27bece92f991b4d4@mail.teambition.com>
2023-01-22 13:07:04 +0800

    update UserCenter 1.用户中心界面和功能已完成 待注册登录功能完成后建立link 2.UI有点丑 后期一起讨论出设计再修改 3.业务不熟接口实在搞不明白了QAQ 大概写了下先交一版

---
+++ src/pages/UserCenter.tsx
@@ -0,0 +1,271 @@
+import { useRef, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Divider, Row, Col, Dialog } from 'tdesign-react';
+import * as $ from "../tools/kit"
+import UserAttach from '../components/user_attach';
+import UpdateUserForm from '../components/update_user_info_form';
+
+
+import online_img from '../assets/online.png';
+
+interface User {
+    "id": number,
+    "username": string,
+    "realName": string,
+    "studentId": string,
+    "email": string,
+    "role": string,
+}
+
+interface User_saving {
+    "exps": number,
+    "coins": number
+}
+
+function UserCenter() {
+    const effectCalled = useRef(false);
+
+    const [user, set_user] = useState<User>({
+        "email": "",
+        "realName": "",
+        "role": "",
+        "username": "",
+        "studentId": "",
+        "id": 0
+    });
+
+    const [user_saving, set_user_saving] = useState<User_saving>({
+        "exps": 0,
+        "coins": 0
+    });
+
+    const [visibleBtn, setVisibleBtn] = useState(false);
+
+    useEffect(() => {
+        if (effectCalled.current) return;
+
+        //获取用户信息
+        axios({
+            method: "get",
+            url: "https://mock.apifox.cn/m1/1898652-0-default/user/detail",
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+                set_user(response.data);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+
+        //获取用户积分金币
+        axios({
+            method: "get",
+            url: "https://mock.apifox.cn/m1/1898652-0-default/user/saving",
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response);
+                set_user_saving(response.data);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+
+        effectCalled.current = true;
+    }, []);
+
+
+
+
+
+    return <$.BackBox>
+        <h1>用户中心</h1>
+        <Divider></Divider>
+
+        {/*用户基本信息*/}
+        <Row
+            gutter={{ xs: 8, sm: 16, md: 24, }}
+        >
+            <Col span={5}>
+                <Row
+
+                    justify='space-between'
+                    align="middle"
+                    style={
+                        {
+                            backgroundColor: 'gray',
+                            padding: '0 10px 0 10px',
+                            height: '40px',
+                            borderRadius: '8px',
+                        }
+                    }
+                >
+                    <Col span={8}>
+                        <div
+                            style={
+                                {
+                                    display: 'flex',
+                                }
+                            }
+                        >
+                            <span
+                                style={
+                                    {
+                                        display: 'block',
+                                        marginRight: '10px',
+                                        fontSize: '26px',
+                                        color: '#ffffff'
+                                    }
+                                }
+                            >
+                                {user.username}
+                            </span>
+                            <span
+                                style={
+                                    {
+                                        display: 'block',
+                                        marginRight: '10px',
+                                    }
+                                }
+                            >
+                                2022.1.18
+                            </span>
+                        </div>
+                    </Col>
+                    <Col span={4}
+                        style={
+                            {
+                                maxHeight: '40px',
+                                display: 'flex',
+                                justifyContent: 'right',
+                            }
+                        }
+                    >
+                        <div
+                            style={
+                                {
+                                    maxHeight: '40px',
+                                    display: 'flex',
+                                }
+                            }
+                        >
+                            <img src={online_img} alt=""
+                                style={
+                                    {
+                                        display: 'block',
+                                        height: '30px',
+                                        objectFit: 'cover',
+                                    }
+                                }
+                            />
+                        </div>
+                    </Col>
+                </Row>
+                <Row justify='start' align='middle'
+                    style={
+                        {
+                            backgroundColor: 'lightgray',
+                            margin: '10px 0 0 0',
+                            padding: '0 10px 0 10px',
+                            height: '30px',
+                            borderRadius: '8px',
+                        }
+                    }
+                >
+                    <Col span={6}>
+                        {user.realName}
+                    </Col>
+                    <Col span={6}>
+                        三年级
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col span={4}
+                style={
+                    {
+                        height: '80px',
+                    }
+                }
+            >
+                <Row
+                    gutter={{ xs: 8, sm: 16, md: 24, }}
+                >
+                    <Col
+                        span={6}
+                        style={
+                            {
+                                backgroundColor: 'lightgray',
+                                height: '80px',
+                                padding: '15px 0 0 15px',
+                                borderRadius: '8px',
+                            }
+                        }
+                    >
+                        <$.Str>
+                            金币数 {user_saving.coins}
+                        </$.Str>
+                        <div style={{ height: '10px' }}></div>
+                        <$.Str>
+                            经验数 {user_saving.exps}
+                        </$.Str>
+                    </Col>
+                    <Col span={6}
+                        style={
+                            {
+                                height: '80px',
+                                paddingTop: '50px'
+                            }
+                        }
+                    >
+                        <button
+                            style={
+                                {
+                                    display: 'block',
+                                    width: '100%',
+                                    height: '30px',
+                                    color: '#ffffff',
+                                    backgroundColor: '#333333',
+                                    border: 'none',
+                                    borderRadius: '8px',
+                                    cursor: 'pointer',
+                                }
+                            }
+                            onClick={() => setVisibleBtn(true)}
+                        >
+                            修改个人资料
+                        </button>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+
+        <Row
+            gutter={{ xs: 8, sm: 16, md: 24, }}
+        >
+            <Col span={9}>
+                <UserAttach />
+            </Col>
+        </Row>
+
+        <Dialog
+            header="个人资料更新"
+            visible={visibleBtn}
+            onEscKeydown={() => setVisibleBtn(false)}
+            onCloseBtnClick={() => setVisibleBtn(false)}
+            onOverlayClick={() => setVisibleBtn(false)}
+            footer={
+                <>
+                </>
+            }
+        >
+            <UpdateUserForm user={user} set_user={set_user}></UpdateUserForm>
+        </Dialog>
+
+    </$.BackBox>
+}
+
+export default UserCenter
