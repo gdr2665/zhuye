84d6bc45889077e28c838ab2439adface73b1a06
aliyun7489356495 <accounts_63a9a18d27bece92f991b4d4@mail.teambition.com>
2023-01-25 18:51:47 +0800

    update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import * as $ from "../tools/kit"
 import React, {useState} from "react";
+import axios from 'axios';
 import {Button, Input, MessagePlugin, Space} from "tdesign-react";
 
 function Login() {
@@ -7,6 +8,20 @@ function Login() {
     const [password, setPassword] = useState('');
     const toLogin = (e: React.MouseEvent) => {
         if (username != "" && password != "") {
+             axios({
+                method:"get",
+                url:"https://mock.apifox.cn/m1/1898652-0-default/user/detail",
+            })
+            .post(
+                'http://127.0.0.1:5000',{
+                    
+                        'username':username,
+                        'password':password
+    
+                }
+              ).then(res=>{
+                alert(res.data)
+              })
         } else {
             MessagePlugin.warning('用户名、密码不能为空');
         }
