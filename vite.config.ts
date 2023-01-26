878e042e68f9733081c07ab6b878b88d3a67c307
Richard Yang <zhongtian.yang@qq.com>
2023-01-26 20:15:52 +0800

    fix CORS error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://tc.yxzl.top',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'https://tc.yxzl.top',
+        changeOrigin: true,
+        ws: true
+      },
+    },
+  },
 })
