640cfb7670ada97465d8184721324ea03595b73c
aliyun7489356495 <accounts_63a9a18d27bece92f991b4d4@mail.teambition.com>
2023-01-28 10:09:44 +0800

    update Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,175 +1,115 @@
 import * as $ from "../tools/kit"
-import React, {useState} from "react";
+import React, { useEffecct } from "react";
 import axios from 'axios';
-import {Button, Cascader, Input, MessagePlugin, Space} from "tdesign-react";
+import { Button, Cascader, Form, Input, MessagePlugin, SubmitContext } from 'tdesign-react'
+import { Axios, type UserRegisterDTO } from '@/tools/api'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { comeFrom } from '@/tools/const'
+import { setLogin, useAppDispatch } from '@/tools/slices'
 
-function Register() {
-    const [stuCode, setStuCode] = useState('');
-    const stuCodeReg = /^[1-9]\d{9}$/g;
-    const stuCodeInputStatus = stuCode == "" ? undefined : !stuCodeReg.test(stuCode) ? 'error' : undefined;
-    const stuCodeTips = stuCodeInputStatus ? '请输入10位学号' : '';
-    const comeFrom = [
-        {
-            children: [
-                {
-                    label: '17班',
-                    value: '1.1'
-                },
-                {
-                    label: '18班',
-                    value: '1.2'
-                },
-                {
-                    label: '19班',
-                    value: '1.3'
-                },
-                {
-                    label: '20班',
-                    value: '1.4'
-                }
-            ],
-            label: '电信 · 大一',
-            value: '1'
-        },
-        {
-            children: [
-                {
-                    label: '21班',
-                    value: '2.1'
-                },
-                {
-                    label: '22班',
-                    value: '2.2'
-                },
-                {
-                    label: '23班',
-                    value: '2.3'
-                },
-                {
-                    label: '24班',
-                    value: '2.4'
-                }
-            ],
-            label: '智科 · 大一',
-            value: '2'
-        },
-        {
-            children: [
-                {
-                    label: '1班',
-                    value: '3.1'
-                },
-                {
-                    label: '2班',
-                    value: '3.2'
-                },
-                {
-                    label: '3班',
-                    value: '3.3'
-                },
-                {
-                    label: '4班',
-                    value: '3.4'
-                },
-                {
-                    label: '5班',
-                    value: '3.5'
-                },
-                {
-                    label: '6班',
-                    value: '3.6'
-                },
-                {
-                    label: '7班',
-                    value: '3.7'
-                },
-                {
-                    label: '8班',
-                    value: '3.8'
-                }
-            ],
-            label: '电管 · 大一',
-            value: '3'
-        }
-    ]
-    const [name, setName] = useState('');
-    const [username, setUsername] = useState('');
-    const usernameInputStatus = username == "" ? undefined : username == "admin" ? 'error' : undefined;
-    const usernameTips = usernameInputStatus ? '这个用户名已被占用' : '';
-    const [password, setPassword] = useState('');
-    const [rewritePassword, setRewritePassword] = useState('');
-    const toRegister = (e: React.MouseEvent) => {
-        axios({
-            method:"get",
-            url:"https://mock.apifox.cn/m1/1898652-0-default/user/detail",
+const { FormItem } = Form
+type errorType = 'error' | 'warning' | undefined
+
+const Register: React.FC = () => {
+  const [form] = Form.useForm()
+  useEffect(() => {
+    form.validate()
+  }, [form])
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+  const stuCode = Form.useWatch('stuCode', form)
+  const name = Form.useWatch('name', form)
+  const username = Form.useWatch('username', form)
+  const email = Form.useWatch('email', form)
+  const password = Form.useWatch('password', form)
+  const rules = {
+    stuCode: [
+      {
+        required: true,
+        message: '必填',
+        type: 'error' as errorType,
+      },
+      { validator: /^[1-9]\d{9}$/g.test },
+    ],
+    name: [
+      {
+        required: true,
+        message: '必填',
+        type: 'error' as errorType,
+      },
+    ],
+    username: [
+      {
+        required: true,
+        message: '必填',
+        type: 'error' as errorType,
+      },
+    ],
+    password: [
+      {
+        required: true,
+        message: '必填',
+        type: 'error' as errorType,
+      },
+    ],
+    rewritePassword: [
+      {
+        required: true,
+        message: '必填',
+        type: 'error' as errorType,
+      },
+      { validator: (val: string) => password == val },
+    ],
+  }
+  const onSubmit = (e: SubmitContext) => {
+    console.log(e)
+    if (e.validateResult === true) {
+      const data: UserRegisterDTO = {
+        email: email,
+        username: username,
+        realName: name,
+        password: password,
+      }
+      Axios.post('/user/register', data)
+        .then(async () => {
+          dispatch(setLogin())
+          await MessagePlugin.info('提交成功')
+          navigate('/')
         })
-        .post(
-           {
-
-                    'studentId':stuCode,
-                    'realName':name,
-                    'username':username,
-                    'password':password
-
-            }
-          ).then(res=>{
-            alert(res)
-          })
-        MessagePlugin.warning(stuCodeInputStatus+'');
-        if (stuCodeInputStatus == undefined && usernameInputStatus == undefined) {
-
-        } else {
-            MessagePlugin.warning('请检查填写是否有误');
-        }
+        .catch((err) => err)
+    } else {
+      void MessagePlugin.warning('请检查填写是否有误')
     }
+  }
 
-    return <$.SmallerBackBox>
-        <$.LargeTitle>注册</$.LargeTitle>
-        <Space direction={"vertical"} style={{width: "100%"}} size={"large"}>
-            <Input
-                value={stuCode}
-                onChange={setStuCode}
-                status={stuCodeInputStatus}
-                tips={stuCodeTips}
-                placeholder="请输入10位学号（激励机制认证用）"
-            />
-            <Cascader
-                lazy
-                loading={false}
-                options={comeFrom}
-                placeholder={"请选择你的组织（激励机制认证用）"}
-                showAllLevels
-            />
-            <Input
-                value={name}
-                onChange={setName}
-                placeholder="请输入姓名（激励机制认证用）"
-            />
-            <Input
-                value={username}
-                onChange={setUsername}
-                status={usernameInputStatus}
-                tips={usernameTips}
-                placeholder="请输入用户名（登录用的账号名）"
-            />
-            <Input
-                type={"password"}
-                value={password}
-                onChange={setPassword}
-                placeholder="请输入密码"
-            />
-            <Input
-                type={"password"}
-                value={rewritePassword}
-                onChange={setRewritePassword}
-                placeholder="请重新输入密码"
-            />
-            <Space>
-                <Button onClick={toRegister}>注册</Button>
-                <Button theme="default" href={"/login"}>登录</Button>
-            </Space>
-        </Space>
-    </$.SmallerBackBox>
-}
-
-export default Register
+  return (
+    <$.SmallerBackBox>
+      <$.LargeTitle>注册</$.LargeTitle>
+      <Form form={form} onSubmit={onSubmit} rules={rules}>
+        <FormItem name={['stuCode']}>
+          <Input placeholder='请输入10位学号（激励机制认证用）' />
+        </FormItem>
+        <FormItem name={['comeFrom']}>
+          <Cascader
+            lazy
+            loading={false}
+            options={comeFrom}
+            placeholder={'请选择你的组织（激励机制认证用）'}
+            showAllLevels
+          />
+        </FormItem>
+        <FormItem name={['name']}>
+          <Input placeholder='请输入姓名（激励机制认证用）' />
+        </FormItem>
+        <FormItem name={['username']}>
+          <Input placeholder='请输入用户名（登录用的账号名）' />
+        </FormItem>
+        <FormItem name={['email']}>
+          <Input placeholder='请输入邮箱' />
+        </FormItem>
+        <FormItem name={['password']}>
+          <Input type='password' placeholder='请输入密码' />
+        </FormItem>
+        <FormItem name={['rewritePassword']}>
+          <Input type='password' placeholder='请重新输入密码' />
+        </FormItem>
